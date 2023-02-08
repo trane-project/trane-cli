1d607ce8e3ee36890aff806f9f3cc4977222bbba
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2023-02-07 22:09:56 -0800

    Add tool  to generate knowledge base courses from config (#53)

---
+++ src/simple_build.rs
@@ -0,0 +1,32 @@
+//! Command line tool that takes a simple knowledge base course configuration file and builds the
+//! course in the current directory.
+
+use anyhow::Result;
+use clap::Parser;
+use std::{env::current_dir, fs};
+use trane::course_builder::knowledge_base_builder::SimpleKnowledgeBaseCourse;
+
+#[derive(Debug, Parser)]
+#[clap(name = "trane")]
+#[clap(author, version, about, long_about = None)]
+pub(crate) struct SimpleBuild {
+    #[clap(help = "The path to the simple knowledge course configuration file to use")]
+    config_file: String,
+
+    #[clap(help = "The directory to which to build the course")]
+    directory: String,
+}
+
+fn main() -> Result<()> {
+    // Parse the command-line arguments.
+    let args = SimpleBuild::parse();
+
+    // Parse the input file and build the course.
+    let config_path = &current_dir()?.join(args.config_file);
+    let simple_course =
+        serde_json::from_str::<SimpleKnowledgeBaseCourse>(&fs::read_to_string(config_path)?)?;
+
+    // Build the course.
+    let directory = &current_dir()?.join(&args.directory);
+    simple_course.build(directory)
+}
