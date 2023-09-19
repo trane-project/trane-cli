1ff213a90871479f5c04d8e14c212072349fed6f
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2023-09-18 19:40:01 -0700

    Upgrade trane to v0.17.0 (#78)

--- build.rs
+++ build.rs
@@ -1,12 +1,9 @@
 use std::{env, path};
 
-use built::Options;
-
 fn main() {
-    let mut opts = Options::default();
-    opts.set_dependencies(true);
-    let manifest_location = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let manifest_var = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let manifest_location = path::Path::new(&manifest_var);
     let dst = path::Path::new(&env::var("OUT_DIR").unwrap()).join("built.rs");
-    built::write_built_file_with_opts(&opts, manifest_location.as_ref(), &dst)
+    built::write_built_file_with_opts(Some(manifest_location), &dst)
         .expect("Failed to acquire build-time information");
 }
